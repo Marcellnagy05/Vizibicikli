2e0d038f4194f74ac0fcdcb5fe214b44297777ae
Marcell Nagy <marcika863@gmail.com>
2023-05-22 11:14:45 +0200

    Előkészületek befejezve

---
+++ kolcsonzes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    public class kolcsonzes
+    {
+        string nev;
+        char jazon;
+        int eOra;
+        int ePerc;
+        int vOra;
+        int vPerc;
+
+        public kolcsonzes(string nev, char jazon, int eOra, int ePerc, int vOra, int vPerc)
+        {
+            this.nev = nev;
+            this.jazon = jazon;
+            this.eOra = eOra;
+            this.ePerc = ePerc;
+            this.vOra = vOra;
+            this.vPerc = vPerc;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public char Jazon { get => jazon; set => jazon = value; }
+        public int EOra { get => eOra; set => eOra = value; }
+        public int EPerc { get => ePerc; set => ePerc = value; }
+        public int VOra { get => vOra; set => vOra = value; }
+        public int VPerc { get => vPerc; set => vPerc = value; }
+    }
+}
